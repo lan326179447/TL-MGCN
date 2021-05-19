2a9234fc554823230d66872ba4875b62810477b5
lan326179447 <53174385+lan326179447@users.noreply.github.com>
2021-05-19 19:55:48 +0800

    Add files via upload

---
+++ TL-MGCN/demo_new.py
@@ -0,0 +1,12 @@
+from wdl import InitParam2
+from wdl import train_experiment2_tr
+
+
+#实例化参数类
+param2 = InitParam2(filename="Datasets\\A1.csv", n_iters=310, batch_size=22)
+#设置记录实验结果的文件
+param2.set_save_record_file(filename="tr_record.csv")
+#设置保存模型文件
+#param2.set_save_model_file(weights_file="tr_A1_trained_weights.pkl", model_file="E:\\example_project\\model\\tr_A1_RF.pkl")
+#训练模型
+train_experiment2_tr(source_weights="model_SD\\trained_weights4AS1.pkl", param=param2)
